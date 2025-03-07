29d871294ae1ace4c32f18d548a69fedb50f2cd5
aldi-013 <lcchampion40@gmail.com>
2025-03-08 01:51:22 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,16 @@
+CREATE TABLE offers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    gift_link TEXT UNIQUE NOT NULL,
+    time_end TEXT NOT NULL,
+    highest_bid INTEGER DEFAULT 0,
+    winner_id INTEGER DEFAULT NULL
+);
 
+CREATE TABLE bids (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    offer_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    username TEXT NOT NULL,
+    bid_amount INTEGER NOT NULL,
+    FOREIGN KEY (offer_id) REFERENCES offers(id)
+);
